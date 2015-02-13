6da67abb44c264996687045f5920b10ae6ca9148
JustinBrown <brown.jt@gmail.com>
2015-02-12 17:16:03 -0700

    Hooked up carousel. Still only partial content and no styles.

---
+++ site/snippets/about.php
@@ -0,0 +1,11 @@
+<div class="about">
+  <p>
+    BLANK PAGE STUDIO is a space for people dedicated to the advancement of innovative practices and ideas in art, architecture, design, and culture. Our mandate is to engage a broad audience throughout Calgary’s communities in generating dialogue about, and active participation in the city’s cultural sphere.
+  </p>
+  <p>
+    Designed as a hub for local emerging creative professionals, the studio was founded in 2013 by visual artist daniel j kirk, co-created by graduate architects Ivan Ostapenko and Studio North, and also includes Market Collective and Intelligent Futures.
+  </p>
+  <p>
+    We welcome collaborations with groups and individuals working to improve communities through thoughtful integration of art and design into the future of the city’s development.
+  </p>
+</div>
---
+++ site/snippets/welcome.php
@@ -0,0 +1,18 @@
+<div class="welcome">
+  <!-- <img src="logo.png" alt=""> -->
+  <div class="contact">
+    <span class="address">
+      1221 B
+      KENSINGTON
+      ROAD, N.W.
+    </span>
+    <span class="city">
+      CALGARY, ALBERTA
+    </span>
+    <span class="other">
+      403.475.1994
+      <a href="/assets/images/bpmap.jpg">DOWNLOAD OUR MAP</a>
+      <a href="https://www.google.ca/maps/place/Blank+Page+Studio/@51.052388,-114.090636,17z/data=!4m2!3m1!1s0x53716fe9a958d97b:0x2943c449e2029829">GOOGLE MAPS LINK</a>
+    </span>
+  </div>
+</div>
